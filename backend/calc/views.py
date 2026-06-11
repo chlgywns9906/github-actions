1ebf6b5974232956df28985c012a43101792a223
user1 <user1@aaa.com>
2026-06-11 14:35:53 +0900

    Update views.py

--- backend/calc/views.py
+++ backend/calc/views.py
@@ -4,7 +4,7 @@ def add(request):
     try:
         num1 = int(request.GET.get("num1", 0))
         num2 = int(request.GET.get("num2", 0))
-        result = num1 - num2
+        result = num1 + num2
     except:
         return JsonResponse({"error": "invalid input"}, status=400)
     
